9562bba4b2cd0065516e27a87fa0ebddf004f868
dreruff <gregoryaruffin@gmail.com>
2017-01-29 15:51:04 -0800

    Initial Commit

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Greg Ruffin on 1/29/17.
+//  Copyright © 2017 Greg Ruffin. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
