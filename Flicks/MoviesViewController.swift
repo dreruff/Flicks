30e7683c5be92f378b8f785dc7b6f7e44f4a97a2
dreruff <gregoryaruffin@gmail.com>
2017-01-29 17:16:52 -0800

    Added loading state and pull to refresh

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -21,21 +22,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-            if let data = data {
-                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                    print(dataDictionary)
-                    
-                    self.movies = dataDictionary["results"] as? [NSDictionary]
-                    self.tableView.reloadData()
-                }
-            }
-        }
-        task.resume()
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        networkRequest()
+        
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(MoviesViewController.refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
+        
+        // add refresh control to table view
+        tableView.insertSubview(refreshControl, at: 0)
 
         // Do any additional setup after loading the view.
     }
@@ -62,7 +58,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let overview = movie["overview"] as! String
         let posterPath = movie["poster_path"] as! String
         
-        let baseUrl = "https://image.tmdb.org/t/p/w500/"
+        let baseUrl = "https://image.tmdb.org/t/p/w342/"
         let imageUrl = NSURL(string: baseUrl + posterPath)
         
         cell.titleLabel.text = title
@@ -72,8 +68,43 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         print("row\(indexPath.row)")
         return cell
     }
-
     
+    func networkRequest() {
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print(dataDictionary)
+                    
+                    self.movies = dataDictionary["results"] as? [NSDictionary]
+                    self.tableView.reloadData()
+        
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    
+                }
+            }
+        }
+        task.resume()
+    }
+
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        // Get new data to update the data source ...
+        networkRequest()
+        
+        // Reload the tableView now that there is new data
+        tableView.reloadData()
+            
+        // Tell the refreshControl to stop spinning
+        refreshControl.endRefreshing()
+
+    }
 
     /*
     // MARK: - Navigation
